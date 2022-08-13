9dc0654cabefe86d28206b4cc3b3ae55c42416bb
Ben Scott <blscott@gmail>
2022-08-13 14:37:39 +1200

    sign up stimulus rewrite

--- app/javascript/src/js/formValidations.js
+++ app/javascript/src/js/formValidations.js
@@ -1,121 +1,37 @@
 /* eslint-disable */
 
-import validate from 'validate.js';
+import { Application } from "stimulus";
+import { ValidationController } from "stimulus-validation"
+// https://github.com/jwald1/stimulus-validation
 
-const constraints = {
-  'user[username]': {
-    length: {
-      minimum: 4,
-      maximum: 20,
-    },
-  },
-  'user[email]': {
-    email: true,
-  },
-  'user[password]': {
-    length: {
-      minimum: 6,
-      maximum: 128,
-    },
-  },
-  'user[password_confirmation]': {
-    equality: {
-      attribute: 'user[password]',
-      message: '^The passwords do not match',
-    },
-  },
-  'user[current_password]': {
-    length: {
-      minimum: 6,
-      maximum: 128,
-    },
-  },
-  'user[learning_goal]': {
-    length: {
-      minimum: 4,
-      maximum: 100,
-    },
-  },
-};
-
-document.addEventListener('DOMContentLoaded', () => {
-  const forms = document.querySelectorAll('.form');
-  if (!forms) return;
-
-  forms.forEach((form) => {
-    // Disable form submission if error
-    form.addEventListener('submit', (ev) => {
-      const formErrors = validate(form, constraints, { fullMessages: false });
-      if (formErrors) {
-        ev.preventDefault();
-        handleFormSubmit(form);
-        ev.stopPropagation();
-      }
-    });
-
-    const inputs = form.querySelectorAll('.form__element');
-
-    inputs.forEach((item) => {
-      item.addEventListener('change', function (ev) {
-        const errors = validate(form, constraints, { fullMessages: false }) || {};
-        showErrorsForInput(this, errors[this.name]);
-      });
-    });
-  });
-});
-
-function handleFormSubmit(form) {
-  const formErrors = validate(form, constraints, { fullMessages: false });
-  if (!formErrors) {
-    form.submit();
-  }
-  showErrors(form, formErrors || {});
-}
-
-function showErrors(form, errors) {
-  const formInputs = form.querySelectorAll('.form__element');
-  formInputs.forEach((item) => {
-    showErrorsForInput(item, errors[item.name]);
-  });
-}
-
-function showErrorsForInput(input, errors) {
-  const inputParent = input.parentNode;
-  const errorWrapper = findErrorWrapper(input) || createErrorWrapper(input, inputParent);
-  const errorDiv = findErrorDiv(input) || createErrorDiv(input, inputParent);
-
-  if (!errors) {
-    errorWrapper.parentNode.insertBefore(input, errorWrapper);
-    errorWrapper.parentNode.removeChild(errorWrapper);
-    errorDiv.parentNode.removeChild(errorDiv);
-    return;
+export default class SignupValidation extends ValidationController {
+  static rules = {
+    email: { presence: { allowEmpty: false }, email: true},
+    username: { presence: { allowEmpty: false } },
+    password: { presence: { allowEmpty: false }, length: {minimum: 6, maximum: 128 } }
   }
 
-  errorWrapper.classList.add('field_with_errors');
-  errorDiv.innerHTML = errors;
-}
+  static validators = { passwordIsConfirmed: { attributes: ['password_confirmation' ]  } }
 
-function findErrorWrapper(input) {
-  if (input.parentNode.classList.contains('field_with_errors')) {
-    return input.parentNode;
+  passwordIsConfirmed({attr, value}) {
+    if (value !== document.getElementById('user_password').value) {
+	    this.errors.add(attr, 'Passwords do not match')
+    }
   }
-  return null;
+
+  afterValidate({ el, attr }) {
+    console.log('yo')
+    this.errorMessageEl(el).textContent = this.errorMessage(attr)
+  }
+
+  errorMessageEl(el) {
+    return el.closest(".form-field").querySelector(".form__error-message")
+  }
+
+  errorMessage(attr) {
+    return this.errors.has(attr) ? this.errors.get(attr)[0] : ""
+  }
 }
 
-function createErrorWrapper(input, inputParent) {
-  const newDiv = document.createElement('div');
-  inputParent.appendChild(newDiv);
-  newDiv.appendChild(input);
-  return newDiv;
-}
-
-function findErrorDiv(input) {
-  return document.querySelector(`.${input.id}`);
-}
-
-function createErrorDiv(input, inputParent) {
-  const divName = document.createElement('div');
-  divName.classList.add('form__error-message', 'push-down', input.id);
-  inputParent.parentNode.insertBefore(divName, inputParent.nextSibling);
-  return divName;
-}
+const application = Application.start();
+application.register('signup-validation', SignupValidation)
